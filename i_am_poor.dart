266768572b08229e83ad2185a3e897e9139e0d8c
tryggos16 <tryggvi.bogason@gmail.com>
2022-03-27 18:10:01 +0000

    i-am-poor 2-1

--- i_am_poor.dart
+++ i_am_poor.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(home: Text('hello you'),));
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.pink,
+        appBar: AppBar(
+          title: const Center(child: Text('i am Poor!')),
+          backgroundColor: Colors.green,
+        ),
+        body: const Center(
+          child:  Image(
+            image: AssetImage('images/no money for you!-2.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
 
